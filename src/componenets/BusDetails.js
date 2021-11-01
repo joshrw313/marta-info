552628fd33d2b13fe1d4bee01d6ad6193531c4bb
Josh White <joshrw313@gmail.com>
2021-11-01 17:52:01 -0400

    fixed variable assigned twice

--- src/componenets/BusDetails.js
+++ src/componenets/BusDetails.js
@@ -17,7 +17,7 @@ const BusDetails = (props) => {
 
 	const findThisBus = () => {
 		const allBusses = Object.keys(props.busData);
-		return allBusses.filter(bus => props.busData[bus].VEHICLE === `${bus}`);
+		return allBusses.filter(Bus => props.busData[Bus].VEHICLE === `${bus}`);
 	};
 
 	const findScheduleAdherence = (adherence) => {
@@ -36,7 +36,7 @@ const BusDetails = (props) => {
 
 	return (  
 		<div className="container-fluid">
-			<div key={bus} className="container-sm" style={ {marginTop: "2rem", color: "white", backgroundColor: "#181716"} } >
+			<div className="container-sm" style={ {marginTop: "2rem", color: "white", backgroundColor: "#181716"} } >
 			<div><h3>{thisBus.DIRECTION}</h3> <h3>{thisBus.TIMEPOINT}</h3> <h3>{findScheduleAdherence(thisBus.ADHERENCE)}</h3></div>
 			</div>
 		</div>
