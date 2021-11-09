f1d7c8bd4a1009408c57414788d73f2f79f00a4b
Josh White <joshrw313@gmail.com>
2021-11-09 16:44:17 -0500

    attempted limiting scope of state awareness to fix rendering issues

--- src/componenets/BusInfo.js
+++ src/componenets/BusInfo.js
@@ -1,5 +1,5 @@
 import { useParams } from "react-router";
-import { useEffect, } from "react";
+import { useEffect, useState } from "react";
 
 const BusInfo = (props) => {
 	const { bus } = useParams();
