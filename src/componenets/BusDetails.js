6bffa4d84e75c6f81eb9094b904bcfd2cb7a1bc6
Josh White <joshrw313@gmail.com>
2021-11-16 17:32:07 -0500

    added check for positionsArray if it is empty

--- src/componenets/BusDetails.js
+++ src/componenets/BusDetails.js
@@ -39,7 +39,7 @@ const BusDetails = (props) => {
 	let positionsArray = []; 
 	position = {lat: Number(data[thisBus].LATITUDE), lng: Number(data[thisBus].LONGITUDE)}; 
 	console.log(position);
-	if (positionsArray[positionsArray.length()] !== position) positionsArray.push(position);
+	if (positionsArray === [] || positionsArray[positionsArray.length()] !== position) positionsArray.push(position);
 	console.log(positionsArray);
 
 	return (
